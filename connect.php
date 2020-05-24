8e247149a2c23575325b6d5208bacaa5940ac121
slawekJasinski <57566000+slawekJasinski@users.noreply.github.com>
2020-05-24 13:00:24 +0200

    Add files via upload
    Connect na serwer

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root','','wikomp_gr1');
+$conn = mysqli_connect('localhost', 'wikomp_gr1','BDWsB2021','wikomp_gr1');
 if (mysqli_connect_errno()) {
     echo "Błąd połączenia z bazą: " . mysqli_connect_error();
     exit();
