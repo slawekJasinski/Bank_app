63516eeacaef39a457571c50fe977750d4563921
slawekJasinski <57566000+slawekJasinski@users.noreply.github.com>
2020-05-30 14:08:51 +0200

    Add files via upload

--- pprodukty.php
+++ pprodukty.php
@@ -1,8 +1,9 @@
 <?php
 {
     session_start();
+    $_SESSION['account_number']='12345678901234567890123456';
+    require_once('connect.php');
     if (isset($_SESSION['username'])) {
-        $conn = mysqli_connect('localhost', 'root', '', 'wikomp_gr1'); //połaczenie z baza
         $username = $_SESSION['username'];
         $id = $_SESSION['id'];
         $sql = "SELECT * FROM `produkty_klienci` left join `produkty` on produkty_klienci.id_produktu=produkty.id_produktu where id_klienta=$id";
@@ -29,5 +30,8 @@ ROW;
 </form>
 <form action="karty.php" method="post">
     <button type="submit">PRzejdzź do kart</button>
+</form>
+<form action="transfer_form.php" method="post">
+    <button type="submit">Może byś tak zrobił przelew?</button>
 </form>
 
