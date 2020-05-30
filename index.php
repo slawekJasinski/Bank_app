bc342651fd9926f2cc953cec5e81a9082976e50f
adriankwasny <62402191+adriankwasny@users.noreply.github.com>
2020-05-30 15:20:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>َAnimated Login Form</title>
+    <title>KASBank</title>
     <link rel="stylesheet" href="style.css">
   </head>
 
