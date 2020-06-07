c876ff7326065b5a085e1606c9724cc618e95be5
slawekJasinski <57566000+slawekJasinski@users.noreply.github.com>
2020-06-07 11:15:33 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+require_once('connect.php');
 function is__account_number_correct($account_number)
 {
     if (empty($account_number) && strlen($account_number) != 26) {
@@ -16,8 +17,29 @@ function is_cvv_correct($cvv_from_user, $cvv_from_system ){
         return false;
     }
 }
+function saldo($nr){
+    $conn = mysqli_connect('localhost', 'wikomp_gr1','BDWsB2021','wikomp_gr1');
+    $sql = sprintf("SELECT `saldo_rachunku`('%s') AS `saldo_rachunku`;", mysqli_real_escape_string($conn, $nr));
+    if ($result = $conn->query($sql)) {
+        $conn->close();
+        $count = $result->num_rows;
+        if ($count == 1) {
+            $amount = $result->fetch_assoc();
+            return $amount['saldo_rachunku'];
+        }
+    }
+}
 function dostepne_srodki($nr){
-return 800;
+    $conn = mysqli_connect('localhost', 'wikomp_gr1','BDWsB2021','wikomp_gr1');
+    $sql = sprintf("SELECT `dostepne_srodki_rachunku`('%s') AS `dostepne_srodki_rachunku`;", mysqli_real_escape_string($conn, $nr));
+    if ($result = $conn->query($sql)) {
+        $conn->close();
+        $count = $result->num_rows;
+        if ($count == 1) {
+            $amount = $result->fetch_assoc();
+            return $amount['dostepne_srodki_rachunku'];
+            }
+        }
 }
 function make_transfer(){
 
--- pprodukty.php
+++ pprodukty.php
@@ -1,15 +1,18 @@
 <?php
-{ 
+{
     session_start();
     $_SESSION['account_number']='12345678901234567890123456';
     require_once('connect.php');
+    require_once('functions.php');
     if (isset($_SESSION['username'])) {
         $username = $_SESSION['username'];
         $id = $_SESSION['id'];
         $sql = "SELECT * FROM `produkty_klienci` left join `produkty` on produkty_klienci.id_produktu=produkty.id_produktu where id_klienta=$id";
         $result = mysqli_query($conn, $sql) or die("Błąd polaczenia" . mysqli_error($conn));
         while ($row = mysqli_fetch_assoc($result)) {
-           echo <<<ROW
+            $saldo=saldo($row['id_produktu']);
+            $dostepne_srodki=dostepne_srodki($row['id_produktu']);
+            echo <<<ROW
       <tr style="color: red;">
         <td>$row[id_produktu_klienta]</td>
         <td>$row[id_klienta]</td>
@@ -18,194 +21,196 @@
         <td>$row[data_aktywacji]</td>
         <td>$row[data_od]</td>
         <td>$row[nazwa_produktu]</td>
+        <td>$saldo</td>
+        <td>$dostepne_srodki</td>   
       </tr>
       </br>
 ROW;
         }
     }
-}  
+}
 ?>
 
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" href="sgb.css" type="text/css">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-        
-        <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <script src="js/jquery.scrollTo.min.js"></script>
-        <script src="js/scroll-up.js"></script>
-	
-        <title>KAS Bank - bankowość elektroniczna</title>
-    </head>
-    
-    <body>
-        <div class="wrapper">
-            <a href="#" class="scrollup"></a>
+    <link rel="stylesheet" href="sgb.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
-            <div class="sidebar">
-                <div class="sidebar-top">
-                    <a href="#"><h2 style="">Pulpit</h2></a>
-                    <ul>
-                        <li><a href="pprodukty.php"><i class="fas fa-home"></i>Strona główna</a></li>
-                        <li><a href="#"><i class="fas fa-briefcase"></i>Twoje produkty</a></li>
-                        <li><a href="karty.php"><i class="fas fa-credit-card"></i>Karty płatnicze</a></li>
-                        <li><a href="twoje_sprawy.php"><i class="fas fa-file-alt"></i>Twoje wnioski</a></li>
-                        <li><a href="Przelew.php"><i class="fas fa-exchange-alt"></i>Przelewy</a></li>
-                        <li><a href="#"><i class="fas fa-history"></i>Historia rachunku</a></li>
-                        <li><a href="#"><i class="fas fa-exclamation-circle"></i>Limity</a></li>
-                        <li><a href="#"><i class="fas fa-ban"></i>Blokady kart</a></li>
-                        <li><a href="#"><i class="fas fa-shopping-basket"></i>Koszyk przelewów</a></li>
-                        <li><a href="#"><i class="fas fa-address-book"></i>Odbiorcy i płatnicy</a></li>
-                        <li><a href="#"><i class="fas fa-history"></i>Historia przelewów</a></li>
-                    </ul> 
-                </div>
-                
-                <div class="sidebar-bottom">
-                    <div class="sidebar-top">
-                        <ul>
-                            <li><a href="#" style="padding: 0; text-align: center; line-height: 50px;">Ustawienia</a></li> 
-                            <li><a href="#" style="padding: 0; text-align: center; line-height: 50px;">Wyloguj</a></li> <!--zrobić forma!!!(lub nie)-->
-                        </ul>
-                    </div>
-                </div>
-            </div>
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/scroll-up.js"></script>
 
-            <div class="main_content">
-                <div class="header">
+    <title>KAS Bank - bankowość elektroniczna</title>
+</head>
 
-                    <div class="logo-header">
-                        <img src="img/logo.png" alt="logo KAS Bank" >
-                        <div style="clear: both;"></div>
-                    </div>
+<body>
+<div class="wrapper">
+    <a href="#" class="scrollup"></a>
 
-                    <div class="sign-out">
+    <div class="sidebar">
+        <div class="sidebar-top">
+            <a href="#"><h2 style="">Pulpit</h2></a>
+            <ul>
+                <li><a href="pprodukty.php"><i class="fas fa-home"></i>Strona główna</a></li>
+                <li><a href="#"><i class="fas fa-briefcase"></i>Twoje produkty</a></li>
+                <li><a href="karty.php"><i class="fas fa-credit-card"></i>Karty płatnicze</a></li>
+                <li><a href="twoje_sprawy.php"><i class="fas fa-file-alt"></i>Twoje wnioski</a></li>
+                <li><a href="Przelew.php"><i class="fas fa-exchange-alt"></i>Przelewy</a></li>
+                <li><a href="#"><i class="fas fa-history"></i>Historia rachunku</a></li>
+                <li><a href="#"><i class="fas fa-exclamation-circle"></i>Limity</a></li>
+                <li><a href="#"><i class="fas fa-ban"></i>Blokady kart</a></li>
+                <li><a href="#"><i class="fas fa-shopping-basket"></i>Koszyk przelewów</a></li>
+                <li><a href="#"><i class="fas fa-address-book"></i>Odbiorcy i płatnicy</a></li>
+                <li><a href="#"><i class="fas fa-history"></i>Historia przelewów</a></li>
+            </ul>
+        </div>
 
-                        <form action="logout.php" method="get">  <!-- form czy a href ? -->
-                            <button class="btn">Wyloguj<i class="fas fa-sign-out-alt"></i></button>
-                        </form>
-                                                
-                    </div>
-                    
-                </div>  
-
-                <div class="info">
-                    <div class="warn">
-                        <div class="msg">Wszelkie e-maile, wiadomości i telefony, w których jesteś proszony o <strong style="color: #fff;">dane autoryzacyjne</strong> typu numer identyfikacyjny, kod dostępu lub kod SMS, traktuj jako <b style="color: #fff;">próbę oszustwa</b>!</div>
-                    </div>
-                    <!--może rozdzielić warn i square (od info)-->
-
-                    <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   <div class="square">
-                        <p>Wnioski</p>
-                        <div class="test">
-                            <a href="twoje_sprawy.php">
-                                <button class="btn2">Zobacz swoje wnioski</button>
-                            </a>
-                        </div>
-                    </div>
-                   
-                </div>
+        <div class="sidebar-bottom">
+            <div class="sidebar-top">
+                <ul>
+                    <li><a href="#" style="padding: 0; text-align: center; line-height: 50px;">Ustawienia</a></li>
+                    <li><a href="#" style="padding: 0; text-align: center; line-height: 50px;">Wyloguj</a></li> <!--zrobić forma!!!(lub nie)-->
+                </ul>
             </div>
         </div>
-    </body>
+    </div>
+
+    <div class="main_content">
+        <div class="header">
+
+            <div class="logo-header">
+                <img src="img/logo.png" alt="logo KAS Bank" >
+                <div style="clear: both;"></div>
+            </div>
+
+            <div class="sign-out">
+
+                <form action="logout.php" method="get">  <!-- form czy a href ? -->
+                    <button class="btn">Wyloguj<i class="fas fa-sign-out-alt"></i></button>
+                </form>
+
+            </div>
+
+        </div>
+
+        <div class="info">
+            <div class="warn">
+                <div class="msg">Wszelkie e-maile, wiadomości i telefony, w których jesteś proszony o <strong style="color: #fff;">dane autoryzacyjne</strong> typu numer identyfikacyjny, kod dostępu lub kod SMS, traktuj jako <b style="color: #fff;">próbę oszustwa</b>!</div>
+            </div>
+            <!--może rozdzielić warn i square (od info)-->
+
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+            <div class="square">
+                <p>Wnioski</p>
+                <div class="test">
+                    <a href="twoje_sprawy.php">
+                        <button class="btn2">Zobacz swoje wnioski</button>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
 </html>
