2b19d386e1089025536d893d71642b7178b5999a
adriankwasny <62402191+adriankwasny@users.noreply.github.com>
2020-06-12 17:26:01 +0200

    adrian

--- register.php
+++ register.php
@@ -8,7 +8,7 @@ session_start();
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="register.css" type="text/css">
+        <link rel="stylesheet" href="css/register.css" type="text/css">
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
         <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
