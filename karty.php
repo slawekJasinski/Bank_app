48d9471fd776c4f3fc9efce6c1b84f11871b6b60
slawekJasinski <57566000+slawekJasinski@users.noreply.github.com>
2020-06-12 22:41:14 +0200

    Add files via upload

--- karty.php
+++ karty.php
@@ -1,14 +1,15 @@
 <?php
-    session_start();
-    require_once('connect.php');
-    if (isset($_SESSION['username'])) {
-        $conn = mysqli_connect('localhost', 'wikomp_gr1', 'BDWsB2021', 'wikomp_gr11'); //połaczenie z baza
-        $username = $_SESSION['username'];
-        $id = $_SESSION['id'];
-        $sql = "SELECT * FROM `produkty_klienci` join `karty` using (id_produktu_klienta) WHERE id_klienta=$id";
-        $result = mysqli_query($conn, $sql) or die("Błąd polaczenia" . mysqli_error($conn));
-        while ($row = mysqli_fetch_array($result)) {
-            echo <<<ROW
+session_start();
+require_once('connect.php');
+require_once('functions.php');
+if (isset($_SESSION['username'])) {
+    $conn = mysqli_connect('localhost', 'wikomp_gr1', 'BDWsB2021', 'wikomp_gr11'); //połaczenie z baza
+    $username = $_SESSION['username'];
+    $id = $_SESSION['id'];
+    $sql = "SELECT * FROM `produkty_klienci` join `karty` using (id_produktu_klienta) WHERE id_klienta=$id";
+    $result = mysqli_query($conn, $sql) or die("Błąd polaczenia" . mysqli_error($conn));
+    while ($row = mysqli_fetch_array($result)) {
+        $string = <<<ROW
       <tr>
         <td>$row[id_produktu_klienta]</td>
         <td>$row[id_klienta]</td>
@@ -19,12 +20,16 @@
         <td>$row[numer_karty]</td>
         <td>$row[data_waznosci]</td>
         <td>$row[CVV]</td>
-        <td>$row[czy_zablokowana]</td>
+        <td>%s</td>
       </tr>
       </br>
 ROW;
-        }
+        $string = sprintf($string, translate($row['czy_zablokowana']));
+        echo $string;
     }
+}else{
+    header('location:index.php');
+}
 ?>
 
 <form action="logout.php" method="post">
