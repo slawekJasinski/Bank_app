a8797a0cd6318b195d7c8f5d52d458b6e857afbb
Sławomir Jasiński <slawomirkaroljasinski@outlook.com>
2020-06-12 23:29:11 +0200

    Update transfer_form.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'wikomp_gr1','BDWsB2021','wikomp_gr11');
-if (mysqli_connect_errno()) {
-    echo "Błąd połączenia z bazą: " . mysqli_connect_error();
-    exit();
-}
-mysqli_set_charset($conn, 'utf8');
-?>
