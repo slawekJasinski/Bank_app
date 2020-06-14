31fd3f2b72da0e7ffe427a4acca16c0136e47f9a
adriankwasny <62402191+adriankwasny@users.noreply.github.com>
2020-06-14 19:06:11 +0200

    adrian

--- pprodukty.php
+++ pprodukty.php
@@ -7,7 +7,18 @@
 <!DOCTYPE html>
     <html lang="en">
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="css/pprodukty.css" type="text/css">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         
+        <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="js/jquery.scrollTo.min.js"></script>
+        <script src="js/scroll-up.js"></script>
+	
+        <title>KAS Bank - bankowość elektroniczna</title>
     </head>
     
     <body>
