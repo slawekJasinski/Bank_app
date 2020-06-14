4901574d6adcd62b3bd4fa01eb49252877f4673d
slawekJasinski <57566000+slawekJasinski@users.noreply.github.com>
2020-06-14 20:10:25 +0200

    Add files via upload

--- transfer_confirm.php
+++ transfer_confirm.php
@@ -10,47 +10,42 @@
     </nav>
 </head>
 <body>
-<?php
-session_start();
-require_once('functions.php');
-$sender = $_SESSION['username'];
-if(isset($_POST['number'])){
-    $account = $_POST['number'];
-}else {
-    $_SESSION['error-trigger'] = 1;
-    $_SESSION['error'] = "Błędne dane. Spróbuj później!";
-}
-if(isset($_POST['credit-card'])){
-    $credit_card = $_POST['credit-card'];
-}
-if(isset($_POST['amount'])){
-    $amount = $_POST['amount'];
-}
-if(isset($_POST['receiver_name'])){
-    $receiver_name = $_POST['receiver_name'];
-}
-if(isset($_POST['receiver_address'])){
-    $_receiver_address = $_POST['receiver_address'];
-}
-if(isset($_POST['date'])){
-    $date = $_POST['date'];
-}
-if(isset($_POST['title'])){
-    $title = $_POST['title'];
-}
-?>
-<h1>Podsumowanie przelewu</h1> <br>
-<?php
-echo $date;
-echo $receiver_name;
-echo $_receiver_address
-echo $credit_card;
-<?php make_transfer($date, $account, $sender, $credit_card, $receiver_name, $_receiver_address, $title, $amount); ?>
-<script>alert("<?php echo bank_name(substr($credit_card, 3, 6)); ?>")</script>
-Konto nadawcy:<?php if(!empty($account)){echo $account;} ?><br>
-Konto odbiorcy:<b><?php echo $credit_card ?><br>
-    Bank:<?php echo bank_name($credit_card) ?></b><br>
-Kwota:<?php echo $amount ?> zł<br>
+    <?php
+    session_start();
+    require_once('functions.php');
+    $sender = $_SESSION['username'];
+    if(isset($_POST['number'])){
+        $account = $_POST['number'];
+        }else{
+        $_SESSION['error-trigger'] = 1;
+        $_SESSION['error'] = "Błędne dane. Spróbuj później!";
+    }
+        if(isset($_POST['credit-card'])){
+            $credit_card = $_POST['credit-card'];
+        }
+        if(isset($_POST['amount'])){
+            $amount = $_POST['amount'];
+        }
+        if(isset($_POST['receiver_name'])){
+            $receiver_name = $_POST['receiver_name'];
+        }
+        if(isset($_POST['receiver_address'])){
+        $_receiver_address = $_POST['receiver_address'];
+        }
+        if(isset($_POST['date'])){
+            $date = $_POST['date'];
+        }
+        if(isset($_POST['title'])){
+            $title = $_POST['title'];
+        }
+    ?>
+    <h1>Podsumowanie przelewu</h1> <br>
+    <?php make_transfer($date, $account, $sender, $credit_card, $receiver_name, $_receiver_address, $title, $amount); ?>
+    <script>alert("<?php echo "Przelew do: ".bank_name(substr($credit_card, 3, 6)); ?>")</script>
+    Konto nadawcy:<?php echo $account ?><br>
+    Konto odbiorcy:<b><?php echo $credit_card ?><br>
+        Bank:<?php echo bank_name($credit_card) ?></b><br>
+    Kwota:<?php echo $amount ?> zł<br>
 
 </body>
 </html>
