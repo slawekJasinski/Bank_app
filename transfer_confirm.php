6a6e2995a79f43ef10b685b2cfc5b7d2f77bee2d
Sławomir Jasiński <slawomirkaroljasinski@outlook.com>
2020-06-18 09:56:06 +0200

    Update transfer_confirm.php

--- transfer_confirm.php
+++ transfer_confirm.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])){
+    header('location:index.php');
+    }
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
@@ -11,10 +17,6 @@
     </head>
     <body>
         <?php
-        session_start();
-        if(!isset($_SESSION['username'])){
-            header('location:index.php');
-        }
         require_once('functions.php');
         $sender = $_SESSION['username'];
         if(isset($_POST['number'])){
